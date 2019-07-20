969ec07192faec6349ca42de976206fb7ba7c2fc
suruomo <www.suruomo@hotmail.com>
2019-07-20 21:19:52 +0800

    对象流

---
+++ src/IO操作/对象类/Main.java
@@ -0,0 +1,22 @@
+package IO操作.对象类;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Hero hero=new Hero();
+        hero.hp=100;
+        hero.name="傻逼";
+        File file=new File("C://WINDOWS");
+        try(FileOutputStream fos=new FileOutputStream(file); ObjectOutputStream oos=new ObjectOutputStream(fos);
+        FileInputStream fis=new FileInputStream(file);ObjectInputStream ois=new ObjectInputStream(fis)
+        ){
+            oos.writeObject(hero);
+            Hero h2 = (Hero) ois.readObject();
+            System.out.println(h2.name);
+            System.out.println(h2.hp);
+        }catch (IOException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
