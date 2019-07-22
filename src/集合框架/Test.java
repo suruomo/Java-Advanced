6c3844914bd8ed193fb98d9135dc2b0857fef65c
suruomo <www.suruomo@hotmail.com>
2019-07-22 17:22:59 +0800

    简单ArrayList

---
+++ src/集合框架/Test.java
@@ -0,0 +1,20 @@
+package 集合框架;
+
+import java.util.ArrayList;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList heros = new ArrayList();
+
+        // 初始化5个对象
+        for (int i = 0; i < 5; i++) {
+            heros.add(new Hero("hero " + i));
+        }
+        for(int i=0;i<heros.size();i++){
+            if(heros.get(i).toString().equals("hero 1")){
+                System.out.println("存在一个name是hero 1的人");
+            }
+        }
+
+    }
+}
