e11f75a45e14e9dab1708eceafb17cff52e88e04
suruomo <www.suruomo@hotmail.com>
2019-07-25 20:18:10 +0800

    二叉树应用

---
+++ src/集合框架/二叉树/Heo二叉树/HeroNode.java
@@ -0,0 +1,15 @@
+package 集合框架.二叉树.Heo二叉树;
+
+public class HeroNode {
+    public String name;
+    public float hp;
+    public HeroNode() {
+
+    }
+    // 增加一个初始化name,hp的构造方法
+    public HeroNode(String name,float hp) {
+        this.hp=hp;
+        this.name = name;
+    }
+
+}
