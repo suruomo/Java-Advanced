be48c83e7915aba907fe6094a1f8ec7c3775434c
suruomo <www.suruomo@hotmail.com>
2019-08-03 09:16:36 +0800

    Lambda

---
+++ src/Lambda/HeroChecker.java
@@ -0,0 +1,7 @@
+package Lambda;
+
+import 集合框架.ArrayList.Hero;
+
+public interface HeroChecker {
+    public boolean test(Hero h);
+}
---
+++ src/Lambda/TestLambdaTwo.java
@@ -0,0 +1,49 @@
+package Lambda;
+
+import 集合框架.ArrayList.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/*
+引入静态方法
+ */
+public class TestLambdaTwo {
+    public static void main(String[] args) {
+        Random r = new Random();
+        List<Hero> heros = new ArrayList<Hero>();
+        for (int i = 0; i < 5; i++) {
+            heros.add(new Hero("hero " + i, r.nextInt(1000), r.nextInt(100)));
+        }
+        System.out.println("初始化后的集合：");
+        System.out.println(heros);
+
+        HeroChecker c = new HeroChecker() {
+            public boolean test(Hero h) {
+                return h.hp>100 && h.damage<50;
+            }
+        };
+
+        System.out.println("使用匿名类过滤");
+        filter(heros, c);
+        System.out.println("使用Lambda表达式");
+        filter(heros, h->h.hp>100 && h.damage<50);
+        System.out.println("在Lambda表达式中使用静态方法");
+        filter(heros, h -> TestLambdaTwo.testHero(h) );
+        System.out.println("直接引用静态方法");
+        filter(heros, TestLambdaTwo::testHero);
+    }
+
+    public static boolean testHero(Hero h) {
+        return h.hp>100 && h.damage<50;
+    }
+
+    private static void filter(List<Hero> heros, HeroChecker checker) {
+        for (Hero hero : heros) {
+            if (checker.test(hero))
+                System.out.print(hero);
+        }
+    }
+
+}
